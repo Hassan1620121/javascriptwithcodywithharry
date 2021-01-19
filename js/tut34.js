0471c31173218aac514cfe1e7ae6e3c5bff6e174
Hassan <Hassan1620121@gmail.com>
2021-01-20 04:38:31 +0500

    tutorial 34.js done

---
+++ js/tut34.js
@@ -0,0 +1,14 @@
+console.log("this is tut 34") ;
+setTimeout(() => {
+
+    for (let index = 0; index < 4005; index++) {
+        const element = index;
+        console.log("This is index no " +index);
+    
+        
+    }
+    
+}, 100);
+
+
+console.log("DOne. printing.")
